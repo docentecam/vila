9aa123dd1e768e0c29015c070615fc4af84ddb8c
Kanra <alixtablet21@gmail.com>
2018-10-30 12:05:33 +0100

    Associacio Update

--- back/views/associacio.php
+++ back/views/associacio.php
@@ -1,28 +1,28 @@
 <div class="AssAllCss">
 	<div class="AssTituCss">Dades Associació:</div>
 	<!-- <div>{{msj}}</div> -->
-	<form>
+	<form ng-submit="submitAss()">
 	 	<div class="form-row">
 			<div class="form-group col-md-8">
 	      		<label for="nomAss">Nom i Cognom</label>
-	      		<input type="text" class="form-control" id="nomAss" placeholder="nom">
+	      		<input type="text" class="form-control" ng-model="ass.nom" id="nomAss" placeholder="nom">
 	    	</div>
 	    	<div class="form-group col-md-4">
 	      		<label for="telefonAss">Telèfon</label>
-	      		<input type="number" class="form-control" id="telefonAss" placeholder="número de telèfon">
+	      		<input type="number" class="form-control" ng-model="telf.nom" id="telefonAss" placeholder="número de telèfon">
 	    	</div>
 	    	<div class="form-group col-md-5">
 		      	<label for="facebookAss">Facebook</label>
 		      		<div class="input-group">
 	        			<div class="input-group-prepend">
-	          				<span class="input-group-text " id="facebookAss"><i class="fab fa-facebook-square"></i></span>
+	          				<span class="input-group-text " id="facebookAss"><i class="fab fa-facebook-square FacebookAssCss"></i></span>
 	        			</div>
-	        			<input type="text" class="form-control" id="facebookAss" placeholder="facebook">
+	        			<input type="text" class="form-control" ng-model="telf.facebook" id="facebookAss" placeholder="facebook">
 	      			</div>
 		    </div>
 		    <div class="form-group col-md-7">
 		      <label for="URLWebAss">URL de la Web</label>
-		      <input type="text" class="form-control" id="URLWebAss" placeholder="URL">
+		      <input type="text" class="form-control" id="URLWebAss" ng-model="telf.URLWeb" placeholder="URL">
 		    </div>
 		    <div class="form-group col-md-6">
 		      	<label for="emailAss">Correu Electronic</label>
@@ -30,41 +30,42 @@
 	        			<div class="input-group-prepend">
 	          				<span class="input-group-text " id="emailAss"><i class="fas fa-at mx-2"></i></span>
 	        			</div>
-	        			<input type="email" class="form-control" id="emailAss" placeholder="correu electronic">
+	        			<input type="email" class="form-control" ng-model="telf.email" id="emailAss" placeholder="correu electronic">
 	      			</div>
 		    </div>
 		    <div class="form-group col-md-6">
 		      <label for="passwordAss">Password</label>
-		      <input type="password" class="form-control" id="passwordAss" placeholder="contrasenya">
+		      <input type="password" class="form-control" id="passwordAss" ng-model="telf.password" placeholder="contrasenya">
 		    </div>
 		    <div class="form-group col-6">
 		      	<label for="horariAss">Horari</label>
-		      	<textarea class="form-control textareaHorariAss" id="horariAss" placeholder="horari"></textarea>
+		      	<textarea class="form-control textareaHorariAss" ng-model="telf.horari" id="horariAss" placeholder="horari"></textarea>
 		    </div>
 		    <div class="form-group col-2">
 		      <label for="latitudAss">Latitud</label>
-		      <input type="number" class="form-control" id="latitudAss" placeholder="latitud">
+		      <input type="number" class="form-control" ng-model="telf.latitud" id="latitudAss"  placeholder="latitud">
 		    </div>
 		    <div class="form-group col-2">
 		      <label for="longitudAss">Longitud</label>
-		      <input type="number" class="form-control" id="longitudAss" placeholder="longitud">
+		      <input type="number" class="form-control" ng-model="telf.longitud" id="longitudAss" placeholder="longitud">
 		    </div>
 		    <div class="form-group col-2">
 		      <label for="keyApiAss">API Key</label>
-		      <input type="text" class="form-control" id="keyApiAss" placeholder="key Api">
+		      <input type="text" class="form-control" id="keyApiAss" ng-model="telf.keyApi" placeholder="key Api">
 		    </div>
 			<div class="form-group col-12">
 		    	<label for="ExplicacioAss">Explicació</label>
-		    	<textarea class="form-control textareaQuiSomAss" id="ExplicacioAss" placeholder="explicació"></textarea>
+		    	<textarea class="form-control textareaQuiSomAss" ng-model="telf.quiSom" id="ExplicacioAss" placeholder="explicació"></textarea>
 	  		</div>	
 	  		<div class="form-group col-12">
 	    		<label for="equipAss">Equip Administratiu</label>
-	    		<textarea class="form-control textareaLGPDAss" id="equipAss" placeholder="equip"></textarea>
+	    		<textarea class="form-control textareaLGPDAss" ng-model="telf.equip" id="equipAss" placeholder="equip"></textarea>
 	  		</div>
 	  		<div class="form-group col-12">
 	    		<label for="LGPDAss">LGPD</label>
-	    		<textarea class="form-control textareaLGPDAss" id="LGPDAss" placeholder="LGPD"></textarea>
+	    		<textarea class="form-control textareaLGPDAss" ng-model="telf.LGPD" id="LGPDAss" placeholder="LGPD"></textarea>
 	  		</div>
 		</div>
+		<input type="button" class="btn btn-primary offset-lg-2" ng-click="submit()" value="Guardar canvis">
 	</form>
 </div>	
