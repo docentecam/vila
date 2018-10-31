99e336af8d6ee924bd170ee2a216d33ad5d1b26e
yaibondi <yaibondi@gmail.com>
2018-10-31 13:58:43 +0100

    ya inserta en la base de datos los formularios.

--- models/contacta.php
+++ models/contacta.php
@@ -1,8 +1,8 @@
 <?php  
 	require("../inc/functions.php");
 	$tbl_contactans="contactans";
-	$tbl_vila="vila";
 	$tbl_solicitutssocis="solicitutssocis";
+	$tbl_vila="vila";
 	if(isset($_POST['acc'])&&$_POST['acc']=='l'){
 		$mySql="SELECT `logoVila`,`facebook`, `nom`, `horari`, `telf`, `email`,`LGPD` FROM $tbl_vila";
 		$connexio=connect();
@@ -17,8 +17,10 @@
 		echo json_encode($rows);	
 	}
 	if(isset($_POST['acc'])&&$_POST['acc']=='i'){
-		$mySql="INSERT INTO $tbl_contactans (`nomContacte`,`cognomContacte`,`tipus`,`email`, `telefon`, `nomEmpresa`,`txtContacte`, `data`) 
-				VALUES  ('".$_POST['nomContacte']."','".$_POST['cognomContacte']."','".$_POST['tipus']."','".$_POST['email']."','".$_POST['telefon']."','".$_POST['nomEmpresa']."','".replaceFromHtml($_POST['txtContacte'])."','".date("Y-m-d H:i:s")."')";
+		$mySql="INSERT INTO $tbl_contactans (`nomContacte`,`cognomContacte`,`tipus`,`email`, `telf`, `nomEmpresa`,`txtContacte`, `data`) 
+				VALUES  ('".$_POST['nomContacte']."','".$_POST['cognomContacte']."','".$_POST['tipus']."','".$_POST['email']."','".$_POST['telf']."','".$_POST['nomEmpresa']."','".replaceFromHtml($_POST['txtContacte'])."','".date("Y-m-d H:i:s")."')";
+
+				// echo $mySql;
 		$connexio=connect();
 		$resultContactans=mysqli_query($connexio,$mySql);
 
@@ -26,15 +28,15 @@
 //TODO VERIFICAR QUE LO INSERTA. $idContactans=mysqli_insert_id
 		
 
-		// $numContac=1;
-		// if($numContac!=0){
+		$numContac=1;
+		if($numContac!=0){
 
-		// 	$envio=sendMail($_POST['email'],"Missatge rebut per la Colla.",donarFormat("Hola, acabem de rebre el teu missatge on deia el següent:<br><br>".$_POST['txtContacte']."<br><br>Moltes gràcies per escriure'ns, et respondrem el més aviat possible així que estigues atent de la teva safata d'entrada.<br><br>Salutacions!"),"si");
-		// 	$estat="ok";
-		// }
-		// else{
-		// 	$estat="ko";
-		// }
+			$envio=sendMail($_POST['email'],"Missatge rebut per la Colla.",donarFormat("Hola, acabem de rebre el teu missatge on deia el següent:<br><br>".$_POST['txtContacte']."<br><br>Moltes gràcies per escriure'ns, et respondrem el més aviat possible així que estigues atent de la teva safata d'entrada.<br><br>Salutacions!"),"si");
+			$estat="ok";
+		}
+		else{
+			$estat="ko";
+		}
 		
 
 		// echo $estat;
@@ -43,38 +45,32 @@
 
 	if(isset($_POST['acc'])&&$_POST['acc']=='insertSolicitutssocis'){
 		$dataActual=date("Y-m-d H:i:s");
-		$newToken="tok".date("smyiHDW");
 
 		$mySql="SELECT `email`,`nom`,`URLWeb` FROM $tbl_vila ";
-				
-		$connexio=connect();
-		$resultSolicitutVila=mysqli_query($connexio,$mySql); 
-		$numSolicitutssocis=mysqli_num_rows($resultSolicitutVila);
-		disconnect($connexio);		
-		$dadesVila=mySqli_fetch_row($resultSolicitutVila);
+		$mySqlInsertSoci="INSERT INTO $tbl_solicitutssocis (`nomComercial`,`sectorComercial`,`adreca`,`telf`,`email`,`data`,`personaContacte`,`horari`)
+				VALUES ('".$_POST['nomComercial']."','".$_POST['sectorComercial']."','".$_POST['adreca']."','".$_POST['telf']."','".$_POST['email']."','".$dataActual."','".$_POST['personaContacte']."','".$_POST['horari']."')";
 
-		if($numSolicitutssocis==0){
-		$mySql="INSERT INTO $tbl_Solicitutssocis (`nomComercial`,`sectorComercial`,`adreca`,`telf`,`email`,`data`,`personaContacte`,`horari`)
-				VALUES ('".$_POST['nomComercial']."','".$_POST['sectorComercial']."','".$_POST['adreca']."','".$_POST['telf']."','".$_POST['email']."','".$_POST['data']."','".$_POST['personaContacte']."','".$_POST['horari']."','".$dataActual."','N','".sha1(md5($newToken))."')";
-
-			$connexio=connect();
-		$resultContactans=mysqli_query($connexio,$mySql);
-
-		disconnect($connexio);
+ 		 $connexio=connect();
+		 $resultVila=mysqli_query($connexio,$mySql); 
+		  $resultSolicitutSoci=mysqli_query($connexio,$mySqlInsertSoci); 
+		 disconnect($connexio);		
+		$dadesVila=mysqli_fetch_row($resultVila);
+echo "ok";
 		
-		$envio=sendMail($_POST['email'],"Benvinguts!",donarFormat("Hola <b>".$_POST['nom']."</b>!<br>Ara que ets un afiliat més a la nostra associació, gaudiràs d'estar al dia de totes les novetats i esdeveniments.<br>No oblidis que qualsevol dubte que puguis tenir, pots contactar amb nosaltres sempre que vulguis.<br>Et convidem a donar-li una ullada a la <a href='".$dadesVila[2]."' target='_blank'>pàgina web</a> i a les nostres xarxes socials on trobaràs molta més informació.<br>Dit això, ens veiem aviat! <br>
-			<form action='".$dadesVila[2]."/cpanel/models/reccontra.php' method='post'>
-			<input name='acc' type='hidden' value='ta'>
-			<input name='afiliat' type='hidden' value='".$newToken."'>
-				<button class='estilButton'>Activació Afiliat</button>
-			</form>	
-			"),"simpat");
-		echo "ok";
-		}
-		else{
-			echo "0";
-		}
+		// $envio=sendMail($_POST['email'],"Benvinguts!",donarFormat("Hola <b>".$_POST['nom']."</b>!<br>Ara que ets un afiliat més a la nostra associació, gaudiràs d'estar al dia de totes les novetats i esdeveniments.<br>No oblidis que qualsevol dubte que puguis tenir, pots contactar amb nosaltres sempre que vulguis.<br>Et convidem a donar-li una ullada a la <a href='".$dadesVila[2]."' target='_blank'>pàgina web</a> i a les nostres xarxes socials on trobaràs molta més informació.<br>Dit això, ens veiem aviat! <br>
+		// 	<form action='".$dadesVila[2]."/cpanel/models/reccontra.php' method='post'>
+		// 	<input name='acc' type='hidden' value='ta'>
+		// 	<input name='afiliat' type='hidden' value='".$newToken."'>
+		// 		<button class='estilButton'>Activació Afiliat</button>
+		// 	</form>	
+		// 	"),"simpat");
+		// echo "ok";
+		// }
+		// else{
+		// 	echo "0";
+		// }
 		
+	// }
 }
 
 ?>
