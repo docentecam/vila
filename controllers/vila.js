45b413c1058e5158cb975797f8c07c9bde782db9
claudiadel <cdelbarr@gmail.com>
2018-11-06 12:23:04 +0100

    botones directori

--- controllers/vila.js
+++ controllers/vila.js
@@ -92,9 +92,17 @@ $(window).on('resize', function() {
 
 
 .controller('DirectoriCtrl',function($scope,$http,$q,$rootScope,$timeout,$window,$document){
+
+	$timeout(function(){
+	console.log("hola");
+				$(".dirButton").height(($("#mapId").height())/4);
+			},3000);
+
 	$(".dirButton").height(($("#mapId").height())/4);
 	console.log(($("#mapId").height())/4);
 	console.log($("#mapId").height());
+
+
 	$scope.llistat=true;
 	$rootScope.cargador=true;
 	var data = new FormData();
@@ -137,7 +145,6 @@ $(window).on('resize', function() {
 	.catch(function(error) {
 		$rootScope.cargador=false;
 		});
->>>>>>> 70ab5d582822b43dd7db3332512ed9b9f4564995
 })
 
 .controller('NoticiesCtrl',function($scope,$http,$q,$rootScope,$timeout,$window,$document){
