ac3675a9d31286f75656785fa3668b78a6152440
yaibondi <yaibondi@gmail.com>
2018-11-07 13:03:10 +0100

    contacta casi listo, nav responsive bien en index.html

--- controllers/vila.js
+++ controllers/vila.js
@@ -219,6 +219,7 @@ $(window).on('resize', function() {
 .controller('ContactaCtrl',function($scope,$http,$q,$rootScope,$timeout,$window,$document){
 	$scope.contactaMissatge=false;
 	$scope.msg="";
+	$scope.fitxaSuccess=true;
 	$scope.contactans={};
 	$scope.contactans.nomContacte="";
 	$scope.contactans.cognomContacte="";
@@ -235,8 +236,6 @@ $(window).on('resize', function() {
 	$scope.contactaSoci.email="";
 	$scope.contactaSoci.personaContacte="";
 	$scope.contactaSoci.horari="";
-	// $scope.fitxaSuccess=true;
-
 	var data = new FormData();
 				data.append("acc","l");
 			var deferred=$q.defer();
@@ -277,6 +276,10 @@ $(window).on('resize', function() {
 		console.log($scope.contactans.checkTipo);
 	}
 	$scope.enviaEmail=function(){
+
+		if($scope.contactans.termes != true){
+				alert("ACCEPTA LES CONDICIONS");
+	}
 
 	//verificar exista email o telefono
 
@@ -344,7 +347,12 @@ $(window).on('resize', function() {
 		}
 }
 $scope.enviaSoci=function(){
+	if($scope.contactaSoci.termes != true){
+				alert("ACCEPTA LES CONDICIONS");
+	}
+
 console.log(isNaN($scope.contactaSoci.telf));
+
 	$scope.llistat=false;
 	if($scope.contactaSoci.email=="" || $scope.contactaSoci.telf==null){
 
@@ -376,7 +384,9 @@ else{
 	.then(function(res){
 		deferred.resolve(res);
 		$rootScope.cargador=false;
+		$scope.fitxaSuccess=false;
 		console.log(res.data);
+		$scope.reload=function(){
 		$scope.contactans=res.data;
 		$scope.contactaSoci.nomComercial="";
 		$scope.contactaSoci.sectorComercial="";
@@ -384,13 +394,17 @@ else{
 		$scope.contactaSoci.telf="";
 		$scope.contactaSoci.email="";
 		$scope.contactaSoci.personaContacte="";
-		$scope.contactaSoci.horari="";
+		$scope.contactaSoci.horari="";						
+		window.location.reload();
+		}
 		if(res.data.trim()=="ok") {
 			$scope.msg="Missatge registrat";
-			$timeout(function(){
-				$scope.contactaMissatge=false;
-				window.location.reload();
-			},3000);
+
+			$scope.msg="Aquest correu electrònic ja està afiliat";
+				$scope.mailIncorrecte=true;
+				$timeout(function(){
+					$scope.mailIncorrecte=false;
+				},3000);
 		}
 		else{
 			$scope.msg="verifica dades";
