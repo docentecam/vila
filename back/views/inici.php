665a012045c28de3a3cfbc76a617e34b34e44efc
@rafatbd Alam Durjoy <rafatinfo4@gmail.com>
2018-11-23 13:25:29 +0100

    arreglo del logo en inici

--- back/views/inici.php
+++ back/views/inici.php
@@ -1,7 +1,6 @@
-<br>
-	<h3 class="iniciCss offset-2 col-8">
-		Manteniment De l'Associacio de la Vila
-	</h3>
+<h3 class="iniciCss offset-2 col-8">
+	Manteniment De l'Associacio de la Vila
+</h3>
 <div class="row">
 	<div class="col">
 		<img class="iniciImgCss" ng-src="../img/{{vila.logoVila}}">
