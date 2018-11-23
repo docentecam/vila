f47698954998d24fa582086ed55d0da10e799864
@rafatbd Alam Durjoy <rafatinfo4@gmail.com>
2018-11-23 13:16:21 +0100

    terminar Inici

--- back/views/inici.php
+++ back/views/inici.php
@@ -1,5 +1,9 @@
+<br>
+	<h3 class="iniciCss offset-2 col-8">
+		Manteniment De l'Associacio de la Vila
+	</h3>
 <div class="row">
 	<div class="col">
-		<img class="iniciImgCss" ng-src="../img/{{logoVila}}">
+		<img class="iniciImgCss" ng-src="../img/{{vila.logoVila}}">
 	</div>
 </div>
