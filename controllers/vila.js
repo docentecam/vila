a0056bf9ef2db69443c6f1fd28fd07b333d4fe77
rta.lyon <rta.lyon@gmail.com>
2018-12-12 12:16:33 +0100

    arreglos menores de vila.js

--- controllers/vila.js
+++ controllers/vila.js
@@ -122,7 +122,6 @@ angular.module('vila')
 		$scope.categoriaassociat=res.data.dadesCategoriaassociat;
 		$scope.galeriaassociats=res.data.dadesGaleriaassociats;
 		$scope.categories=res.data.dadesCategories;
-		console.log($scope.categoriaassociat);
 	})
 	.catch(function(error) {
 		$rootScope.cargador=false;
@@ -240,7 +239,6 @@ angular.module('vila')
 	.then(function(res){
 		deferred.resolve(res);
 		$rootScope.cargador=false;
-		console.log(res.data.dadesFiramar[0]);
 		$scope.firamar=res.data.dadesFiramar[0];
 		$scope.galeriafiramar=res.data.dadesGaleriafiramar;
 		$scope.activitatsfiramar=res.data.dadesActivitatsfiramar;
@@ -394,7 +392,6 @@ angular.module('vila')
 				$scope.contactans.checkTipo="Empresa";
 				
 			}
-		console.log($scope.contactans.checkTipo);
 	}
 
 	$scope.enviaEmail=function(){
@@ -439,7 +436,6 @@ angular.module('vila')
 		.then(function(res){
 			deferred.resolve(res);
 			$rootScope.cargador=false;
-			console.log(""+res.data);
 			$scope.contactans.nomContacte="";
 			$scope.contactans.cognomContacte="";
 			$scope.contactans.checkTipo="";
@@ -503,7 +499,6 @@ $scope.enviaSoci=function(){
 	.then(function(res){
 		deferred.resolve(res);
 		$scope.fitxaSuccess=false;
-		console.log(res.data);
 		if(res.data.trim()=="ok") {
 			$scope.msg="Missatge registrat";
 		}
@@ -520,7 +515,6 @@ $scope.enviaSoci=function(){
 	}
 }
 	$scope.recarga=function(){
-		console.log("Intentamos");
 			$scope.contactaSoci.nomComercial="";
 			$scope.contactaSoci.sectorComercial="";
 			$scope.contactaSoci.adreca="";
